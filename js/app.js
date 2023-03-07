e77f7ea87a095a829261d88d27cfe9a6c2915c2a
Parselinho <parselinho@gmail.com>
2023-03-07 14:33:41 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 const restartBtn = document.getElementById('btn__reset');
 const phraseUL = document.querySelector('#phrase > ul');
 const overlay = document.getElementById('overlay');
+const divButtons = document.querySelector('#qwerty');
 let game;
 
 //function and eventListener to start new game : 
@@ -10,6 +11,19 @@ function newGame() {
 }
 
 restartBtn.addEventListener('click', newGame);
+
+
+// //function and eventListener for the key buttons : 
+// function pressedKey(e) {
+
+// }
+
+divButtons.addEventListener('click', (e) => {
+    if (e.target.tagName === 'BUTTON') {
+        console.log(handleInteraction(button));
+    }
+});
+
 
 // const game = new Game();
 // game.startGame();
