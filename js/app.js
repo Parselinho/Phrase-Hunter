0342199487ec6fcdc4b99869cba478aaeab3b7b0
Parselinho <parselinho@gmail.com>
2023-03-07 14:10:11 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,15 +1,25 @@
 const restartBtn = document.getElementById('btn__reset');
+const phraseUL = document.querySelector('#phrase > ul');
+const overlay = document.getElementById('overlay');
+let game;
 
+//function and eventListener to start new game : 
+function newGame() {
+    game = new Game().startGame();
+    game.activePhrase.checkLetter('a')
+}
+restartBtn.addEventListener('click', newGame);
 
-
-
+// const game = new Game();
+// game.startGame();
+// console.log(`Active Phrase - phrase: ${game.activePhrase.phrase}`);
 
 // const game = new Game();
 // const randomPhrase = game.getRandomPhrase();
 // const phrase = new Phrase(randomPhrase.phrase);
 // phrase.addPhraseToDisplay();
-const game = new Game();
-game.getRandomPhrase().addPhraseToDisplay();
+
+
 // const game = new Game();
 // game.phrases.forEach((phrase, index) => {
 // console.log(`Phrase ${index} - phrase: ${phrase.phrase}`);
