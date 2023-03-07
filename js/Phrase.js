63b10924c2470940e7cd6f516940111e4e7af2b4
Parselinho <parselinho@gmail.com>
2023-03-07 14:10:16 +0200

    Update Phrase.js

--- js/Phrase.js
+++ js/Phrase.js
@@ -7,18 +7,41 @@ class Phrase {
     * Display phrase on game board
     */
     addPhraseToDisplay() {
-        const phraseUL = document.querySelector('#phrase > ul');
         for (let i = 0; i < this.phrase.length; i++) {
             const li = document.createElement('li');
             if (this.phrase[i] === ' ') {
+                li.classList.add('hide');
                 li.classList.add('space');
             } else {
+                li.classList.add('hide');
                 li.classList.add('letter');
+                li.classList.add(this.phrase[i]);
+                li.textContent = this.phrase[i];
             }
             phraseUL.append(li);
         }
     }
 
+    // `checkLetter()`: Checks to see if the letter selected by the player matches a letter in the phrase.
+    //  and Reveals the letter(s) on the board that matches the  player's selection.
+    checkLetter(letter) {
+        const letterLI = document.querySelectorAll('.letter');
+        let isMatch = false;
+        for (let i =0; i < letterLI.length; i++) {
+        if (letterLI[i].textContent === letter) {
+            letterLI[i].classList.remove('hide');
+            letterLI[i].classList.add('show');
+            isMatch = true;
+        }
+        }
+        return isMatch;
+    }
+
+    // showMatchedLetter() {
+
+    // }
+
 }
+
 
 
